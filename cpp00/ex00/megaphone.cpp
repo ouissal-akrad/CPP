d0ae790cb701bb79e1e405bc62c2246f5e432bb8
ouissal-akrad <=>
2023-10-12 21:34:56 +0100

    first try

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,23 +6,27 @@
 /*   By: ouakrad <ouakrad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 16:58:17 by ouakrad           #+#    #+#             */
-/*   Updated: 2023/10/12 21:13:56 by ouakrad          ###   ########.fr       */
+/*   Updated: 2023/10/12 21:34:19 by ouakrad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
 	if (ac < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	for (int i = 1; i < ac; i++)
+	else
 	{
-		// store in std::string
-		for (int j = 0; av[i][j]; ++j)
-			av[i][j] = std::toupper(av[i][j]);
-		std::cout << av[i];
+		for (int i = 1; i < ac; i++)
+		{
+			std::string arg = av[i]; 
+			for (size_t j = 0; j < arg.length(); j++)
+				arg[j] = std::toupper(arg[j]); 
+			std::cout << arg;
+		}
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;
 	return (0);
 }

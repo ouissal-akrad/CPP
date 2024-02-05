72ad98f5f9d6dd8c437d68b88fc2abc9e7f7304f
ouissal-akrad <=>
2024-02-05 18:34:11 +0100

    cpp::09::ex02

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,14 +6,14 @@
 /*   By: ouakrad <ouakrad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 16:33:15 by ouakrad           #+#    #+#             */
-/*   Updated: 2024/02/05 17:54:02 by ouakrad          ###   ########.fr       */
+/*   Updated: 2024/02/05 18:30:55 by ouakrad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
 void	displaySequence(const std::vector<int> &sequence,
-						const std::string &message)
+		const std::string &message)
 {
 	std::cout << message << ": ";
 	for (std::vector<int>::const_iterator it = sequence.begin(); it != sequence.end(); ++it)
@@ -24,8 +24,7 @@ void	displaySequence(const std::vector<int> &sequence,
 }
 
 void	displayTiming(const std::string &containerName,
-					const clock_t &startTime,
-					const clock_t &endTime)
+		const clock_t &startTime, const clock_t &endTime)
 {
 	std::cout << "Time to process a range of " << containerName << " : "
 				<< double(endTime - startTime) / CLOCKS_PER_SEC
@@ -60,7 +59,7 @@ int	customPartition(std::vector<int> &c, int start, int end, int pivotIndex)
 bool	hasDuplicates(const std::vector<int> &c)
 {
 	std::vector<int> sortedCopy = c;
-	// Create a copy to avoid modifying the original vector
+		// Create a copy to avoid modifying the original vector
 	std::sort(sortedCopy.begin(), sortedCopy.end());
 	for (size_t i = 1; i < sortedCopy.size(); ++i)
 	{
@@ -71,11 +70,12 @@ bool	hasDuplicates(const std::vector<int> &c)
 	}
 	return (false); // No duplicates found
 }
-
+bool comparePairs(const std::pair<int, int>& pair1, const std::pair<int, int>& pair2)
+{
+    return pair1.first < pair2.first;
+}
 void	fordJohnsonMergeInsertionSort(std::vector<int> &c, int start, int end)
 {
-	int	lastElement;
-
 	if (start >= end)
 		return ; // Base case for single element or invalid range
 	// Base case for two elements
@@ -86,30 +86,31 @@ void	fordJohnsonMergeInsertionSort(std::vector<int> &c, int start, int end)
 		return ;
 	}
 	// // Handle odd number of elements
-	lastElement = 0;
-	if ((end - start + 1) % 2 != 0)
-		lastElement = c[end];
-	std::cout << "lastElement " << lastElement << std::endl;
-	// Sort pairs
-	std::vector<std::pair<int, int>> vect;
-	for (int i = start; i < end; i += 2)
-	{
-		if (c[i] > c[i + 1])
-			std::swap(c[i], c[i + 1]);
-		std::pair<int, int> pair = std::make_pair(c[i], c[i + 1]);
-		vect.push_back(pair);
-	}
-	for (std::vector<std::pair<int,
-			int>>::const_iterator it = vect.begin(); it != vect.end(); ++it)
-	{
-		std::cout << it->first << " " << it->second << std::endl;
-	}
+	int lastElement = 0;
+    if ((end - start + 1) % 2 != 0)
+        lastElement = c[end];
+    std::cout << "lastElement " << lastElement << std::endl;
+   // Sort pairs
+    std::vector<std::pair<int, int> > vect;
+    for (int i = start; i < end; i += 2)
+    {
+        if (c[i] < c[i + 1])
+            std::swap(c[i], c[i + 1]);
+        std::pair<int, int> pair = std::make_pair(c[i], c[i + 1]);
+        vect.push_back(pair);
+    }
+    // Sort pairs based on the first element
+    std::sort(vect.begin(), vect.end(), comparePairs);
+    for (std::vector<std::pair<int, int> >::const_iterator it = vect.begin(); it != vect.end(); ++it)
+    {
+        std::cout << it->first << " " << it->second << std::endl;
+    }
 }
 
 void	performSorting(std::vector<int> sequence)
 {
-	clock_t startTime;
-	clock_t endTime;
+	clock_t	startTime;
+	clock_t	endTime;
 
 	startTime = clock();
 	fordJohnsonMergeInsertionSort(sequence, 0, sequence.size() - 1);
